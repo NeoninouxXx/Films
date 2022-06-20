8aaa3e8082ba01609c7b29b8ce2fb014512217b5
xavier turpin <turpin182@gmail.com>
2022-06-20 21:03:47 +0400

    version alpha du projet gestion de film

---
+++ assets/js/script.js
@@ -0,0 +1,63 @@
+const formFilms = document.querySelector(".formFilms");
+const filmListe = document.querySelector(".filmListe");
+const tbody = document.querySelector(".tbody");
+const container = document.querySelector(".alerte");
+
+class Film {
+  constructor(titre, acteur, année) {
+    this.titre = titre;
+    this.acteur = acteur;
+    this.année = année;
+  }
+  adFilmToList(film) {
+    const row = document.createElement("tr");
+    row.innerHTML = ` <td>${this.titre}</td>
+                      <td>${this.acteur}</td>
+                      <td>${this.année}</td>
+                      <td><button class="deleted">X</td>`;
+    tbody.appendChild(row);
+  }
+  clearField(film) {
+    document.getElementById("titre").value = "";
+    document.getElementById("acteur").value = "";
+    document.getElementById("année").value = "";
+  }
+  deletedFilm(target) {
+    if (target.className === "deleted") {
+      target.parentElement.parentElement.remove();
+    }
+  }
+}
+
+formFilms.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const titre = document.getElementById("titre").value;
+  const acteur = document.getElementById("acteur").value;
+  const année = document.getElementById("année").value;
+
+  const film = new Film(titre, acteur, année);
+
+  if (titre === "" || acteur === "" || année === "") {
+    const alert = document.createElement(`div`);
+    alert.innerHTML = `<p class="error"> Erreur de saissi </p>`;
+    container.appendChild(alert);
+    setTimeout(() => {
+      alert.remove();
+    }, 3000);
+  } else {
+    film.adFilmToList(film);
+    film.clearField(film);
+    const alert = document.createElement(`div`);
+    alert.innerHTML = `<p class="success"> Film ajouté avec sucess! </p>`;
+    container.appendChild(alert);
+    setTimeout(() => {
+      alert.remove();
+    }, 3000);
+  }
+});
+
+filmListe.addEventListener("click", (e) => {
+  const film = new Film();
+  film.deletedFilm(e.target);
+});
+
